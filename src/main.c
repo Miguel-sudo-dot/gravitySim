bf803baca6ecf7bdb6fb4a189cd9fca0360a70f9
Miguel <miguelnoesmiguel10@gmail.com>
2026-04-02 19:41:59 +0200

    Bouncing ball

--- src/main.c
+++ src/main.c
@@ -1,16 +1,32 @@
 #include "raylib.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+typedef struct{
+    float x, y;
+    float velocity;
+    float radius;
+    Color color;
+    //float mass;
+}Object;
+
+Object InitialiseObject(float x, float y, float velocity, float radius, Color color){
+    Object object;
+    object.x = x;
+    object.y = y;
+    object.velocity = velocity;
+    object.radius = radius;
+    object.color = color;
+    return object;
+}
+
 int main(void)
 {
     // Initialization
     const int screenWidth = 800;
     const int screenHeight = 800;
-    int y = 0;
+    Object object = InitialiseObject(400, 0, 0, 23, WHITE);
+    float acceleration = 0.25;
 
-    InitWindow(screenWidth, screenHeight, "Gravedad");
+    InitWindow(screenWidth, screenHeight, "Gravity");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -20,15 +36,21 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-        y++;
+        object.velocity += acceleration;
+        object.y += object.velocity;
+
+        if(object.y+object.radius >= screenHeight){
+            object.y = screenHeight-object.radius;
+            object.velocity*=-1;
+        }
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(DARKBLUE);
-            DrawCircle(400, y, 23, RED);
+            ClearBackground(BLACK);
+            DrawCircle(object.x, object.y, object.radius, object.color);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
