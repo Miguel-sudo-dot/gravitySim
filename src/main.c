fde3134d6bc6faebb0a62d83fef05d25fb2ee8a0
Miguel <miguelnoesmiguel10@gmail.com>
2026-04-03 12:42:38 +0200

    Fix Bugs

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 
 #define NUMBER_OBJECTS 2
 #define G 398600 // km^3 / (masas terrestre * s^2)
-#define SCALE_FACTOR 100 // 1px -> 1000km
+#define SCALE_FACTOR 100 // 1px -> 100km
 
 typedef struct{
     float x, y;
@@ -29,8 +29,8 @@ int main(void){
     const int screenWidth = 800;
     const int screenHeight = 800;
     Object object[NUMBER_OBJECTS];
-    object[0] = InitialiseObject(400, 0, (Vector2){200,0}, 10, WHITE, 1*1000); 
-    object[1] = InitialiseObject(400, 400, (Vector2){0,0}, 30, WHITE, 10*1000); 
+    object[0] = InitialiseObject(400, 0, (Vector2){200,0}, 10, GRAY, 1*1000); 
+    object[1] = InitialiseObject(400, 400, (Vector2){0,0}, 30, RED, 10*1000); 
 
     InitWindow(screenWidth, screenHeight, "Gravity");
 
@@ -42,18 +42,22 @@ int main(void){
         // Update
         //----------------------------------------------------------------------------------
         float dt = GetFrameTime()*50;
-        float dx = (object[1].x-object[0].x)*SCALE_FACTOR;
-        float dy = (object[1].y-object[0].y)*SCALE_FACTOR;
-        float module = sqrt(dx*dx + dy*dy);
-        float force = ((G*object[0].mass*object[1].mass)/(module*module));
-        float acceleration = force/object[0].mass;
-        Vector2 vu = {dx/module, dy/module};
-        object[0].velocity.x += acceleration*vu.x*dt;
-        object[0].velocity.y += acceleration*vu.y*dt;
-        object[0].x += object[0].velocity.x*dt/SCALE_FACTOR;
-        object[0].y += object[0].velocity.y*dt/SCALE_FACTOR;
-
-        object[1].x += object[1].velocity.x;
+        for(int i=0; i<NUMBER_OBJECTS; i++){
+            for(int j=0; j<NUMBER_OBJECTS; j++){
+                if(j!=i){
+                    float dx = (object[j].x-object[i].x)*SCALE_FACTOR;
+                    float dy = (object[j].y-object[i].y)*SCALE_FACTOR;
+                    float module = sqrt(dx*dx + dy*dy);
+                    float force = ((G*object[i].mass*object[j].mass)/(module*module));
+                    float acceleration = force/object[i].mass;
+                    Vector2 vu = {dx/module, dy/module};
+                    object[i].velocity.x += acceleration*vu.x*dt;
+                    object[i].velocity.y += acceleration*vu.y*dt;
+                }
+            }
+            object[i].x += object[i].velocity.x*dt/SCALE_FACTOR;
+            object[i].y += object[i].velocity.y*dt/SCALE_FACTOR;
+        }
 
         //----------------------------------------------------------------------------------
 
